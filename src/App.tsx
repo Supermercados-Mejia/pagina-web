1acbcda10af5fdd039f91860c0a6597305e0d816
Juan José Eusebio <juanerpgr@gmail.com>
2025-05-16 11:43:12 -0700

    feat: agregar driver.js como dependencia y su CSS en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,13 +26,36 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 import '@ionic/react/css/palettes/dark.class.css';
 
+import "driver.js/dist/driver.css";
+
 // Generar conjunto de todas las rutas válidas
 const allValidPaths = [...navigationAdmin, ...navigationUser].map((item: any) => item.page && item.href);
 const validPathsSet = new Set(allValidPaths);
 
 setupIonicReact({ mode: 'ios' });
 
+
 const App: React.FC = () => {
+
+  /*
+  import { driver } from "driver.js";
+  
+  const driverObj = driver({
+    showProgress: true,
+    steps: [
+      { element: '#tour-example', popover: { title: 'Animated Tour Example', description: 'Here is the code example showing animated tour. Let\'s walk you through it.', side: "left", align: 'start' } },
+      { element: 'code .line:nth-child(1)', popover: { title: 'Import the Library', description: 'It works the same in vanilla JavaScript as well as frameworks.', side: "bottom", align: 'start' } },
+      { element: 'code .line:nth-child(2)', popover: { title: 'Importing CSS', description: 'Import the CSS which gives you the default styling for popover and overlay.', side: "bottom", align: 'start' } },
+      { element: 'code .line:nth-child(4) span:nth-child(7)', popover: { title: 'Create Driver', description: 'Simply call the driver function to create a driver.js instance', side: "left", align: 'start' } },
+      { element: 'code .line:nth-child(18)', popover: { title: 'Start Tour', description: 'Call the drive method to start the tour and your tour will be started.', side: "top", align: 'start' } },
+      { element: 'a[href="/docs/configuration"]', popover: { title: 'More Configuration', description: 'Look at this page for all the configuration options you can pass.', side: "right", align: 'start' } },
+      { popover: { title: 'Happy Coding', description: 'And that is all, go ahead and start adding tours to your applications.' } }
+    ]
+  }); 
+
+  driverObj.drive();
+  */
+
   const history = useHistory();
   const location = useLocation();
 
@@ -45,12 +68,7 @@ const App: React.FC = () => {
   const checkRedirect = useCallback(() => {
     const currentPath = location.pathname;
 
-    if (!currentBranch) {
-      // Usuario no autenticado: redirigir solo si está en ruta válida o raíz
-      /* if (validPathsSet.has(currentPath) || currentPath === '/') {
-        history.replace('/');
-      } */
-    } else {
+    if (currentBranch) {
       // Usuario autenticado: redirigir desde raíz a dashboard');
       if (currentPath === '/') {
         history.replace('/dashboard');
