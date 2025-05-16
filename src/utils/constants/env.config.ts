1d556244fc39b7ab347ab9e8f4fa18e17f96e0f6
Juan José Eusebio <juanerpgr@gmail.com>
2025-05-16 10:28:34 -0700

    feat: agregar api_landing a la configuración de entorno y actualizar su uso

--- src/utils/constants/env.config.ts
+++ src/utils/constants/env.config.ts
@@ -2,6 +2,7 @@
 type EnvConfigType = {
   api: string;
   api_int: string;
+  api_landing: string;
   mode: string;
   itemsPerPage: number;
 };
@@ -17,13 +18,16 @@ export const EnvConfig = (): EnvConfigType => {
 
   const api_int =
     process.env.REACT_PUBLIC_API_URL_INT || "http://localhost:5000/api/";
+
   const api_landing =
     process.env.REACT_PUBLIC_API_URL_LANIDNG || "http://localhost:5000/api/";
+
   const itemsPerPage = parseInt(process.env.ITEMS_PER_PAGE || "10", 10); // Fallback a 10 si no está definido
 
   return {
     api,
     api_int,
+    api_landing,
     mode,
     itemsPerPage,
   };
