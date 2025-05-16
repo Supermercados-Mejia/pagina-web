0c6970356512bb2dd3a4aa5b78ef99d0631e688e
Juan José Eusebio <juanerpgr@gmail.com>
2025-05-16 10:22:41 -0700

    feat: agregar variable de entorno REACT_PUBLIC_API_URL_LANIDNG y actualizar su uso en la configuración

--- src/utils/constants/env.config.ts
+++ src/utils/constants/env.config.ts
@@ -17,7 +17,8 @@ export const EnvConfig = (): EnvConfigType => {
 
   const api_int =
     process.env.REACT_PUBLIC_API_URL_INT || "http://localhost:5000/api/";
-
+  const api_landing =
+    process.env.REACT_PUBLIC_API_URL_LANIDNG || "http://localhost:5000/api/";
   const itemsPerPage = parseInt(process.env.ITEMS_PER_PAGE || "10", 10); // Fallback a 10 si no está definido
 
   return {
