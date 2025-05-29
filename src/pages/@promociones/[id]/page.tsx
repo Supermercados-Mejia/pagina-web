8638f4611c638ab92e70b0a4211d1a0a52187f9e
Juan José Eusebio <juanerpgr@gmail.com>
2025-05-29 16:22:36 -0700

    feat: agregar pantalla de carga y mejorar manejo de iconos en el menú

---
+++ src/pages/@promociones/[id]/page.tsx
@@ -0,0 +1,69 @@
+import Footer from "@/template/footer";
+import { PageProps } from "@/utils/types/page";
+import { IonContent, IonHeader, IonToolbar, IonTitle, IonSegment, IonLabel, IonSegmentButton, IonButton } from "@ionic/react";
+import { promociones, combos } from "../data/example"; // Asegúrate de importar combos
+import { useState } from "react";
+import { ProductoCard, ComboCard } from "../components/card-promociones";
+
+export default function PromocionesID({ onScroll }: PageProps) {
+    // Filtrar datos según la selección
+    const filteredData = combos;
+
+    return (
+        <IonContent
+            fullscreen
+            scrollEvents
+            onIonScroll={(e) => {
+                const isScrolled = e.detail.scrollTop > 20;
+                onScroll?.(isScrolled);
+            }}
+        >
+            <IonHeader
+                collapse="condense"
+                className="custom-toolbar z-50 -top-16">
+                <IonToolbar>
+                    <IonTitle
+                        size="large"
+                        className="text-white font-medium tracking-tight">
+                        <span className="text-6xl">Liz</span>
+                    </IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <main className="w-full min-h-[77vh] px-4 sm:px-6 lg:px-8 pb-7">
+
+                <div className="max-w-6xl mx-auto">
+                    <header className="text-center">
+                        <h1 className="text-3xl md:text-4xl font-bold text-gray-900 mb-3">Promociones</h1>
+
+                        <label className="space-y-1">
+                            <p className="text-gray-600 text-lg">Aprovecha nuestras ofertas y combos</p>
+                            <p className="text-gray-600 text-lg">Conoce nuestro nuevo catalogo...</p>
+                        </label>
+                    </header>
+
+                    <section aria-labelledby="promotions-heading" className="mt-6">
+                        <h2 id="promotions-heading" className="sr-only">Promociones y Combos</h2>
+
+                        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                            {filteredData.length > 0
+                                ? filteredData.map((item: any) => (
+                                    item.nombreCombo
+                                        ? <ComboCard combo={item} key={item.id || item.nombreCombo} />
+                                        : <ProductoCard promocion={item} key={item.id || item.nombre} />
+                                ))
+                                : (
+                                    <div className="m-auto col-span-3 text-center py-8">
+                                        <p className="text-gray-500 italic">
+                                            Actualmente no hay promociones disponibles, vuelve en otro momento.
+                                        </p>
+                                    </div>
+                                )
+                            }
+                        </ul>
+                    </section>
+                </div>
+            </main>
+            <Footer />
+        </IonContent>
+    )
+}
