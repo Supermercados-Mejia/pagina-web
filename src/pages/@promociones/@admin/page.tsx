20ec43c1f05e6742d9442b547ab1dcf80c846db3
Juan José Eusebio <juanerpgr@gmail.com>
2025-05-30 14:24:31 -0700

    feat: agregar modal para añadir promociones y mejorar visualización de datos en PromocionesAdmin

--- src/pages/@promociones/@admin/page.tsx
+++ src/pages/@promociones/@admin/page.tsx
@@ -1,12 +1,23 @@
 import MainForm from "@/components/form/main-form";
 import Footer from "@/template/footer";
 import { PageProps } from "@/utils/types/page";
-import { IonButton, IonContent, IonHeader, IonLabel, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonFabButton, IonHeader, IonLabel, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from "@ionic/react";
 import { useState } from "react";
 import { promocionesField } from "../utils/promociones-field";
+import { ModalAddPromotion } from "../components/modal-add-promotion";
+import { Plus } from "lucide-react";
+import { combos, promociones } from "../data/example";
+import { ComboCard, ProductoCard } from "../components/card-promociones";
 
 export default function PromocionesAdmin({ onScroll }: PageProps) {
     const [selectedType, setSelectedType] = useState<string>('combos');
+    const [isOpen, setIsOpen] = useState(false);
+
+    const filteredData =
+        selectedType === 'combos'
+            ? combos
+            : promociones;
+
     return (
         <IonContent
             fullscreen
@@ -27,8 +38,10 @@ export default function PromocionesAdmin({ onScroll }: PageProps) {
                     </IonTitle>
                 </IonToolbar>
             </IonHeader>
+            <IonFabButton color={"tertiary"} onClick={() => setIsOpen(true)} size="small" className="size-12 fixed right-4 bottom-14 z-50 ">
+                <Plus />
+            </IonFabButton>
             <main className="w-full min-h-[77vh] px-4 sm:px-6 lg:px-8 pb-7">
-
                 <div className="max-w-6xl mx-auto">
                     <header className="text-center">
                         <h1 className="text-3xl md:text-4xl font-bold text-gray-900 mb-3">Promociones</h1>
@@ -61,15 +74,26 @@ export default function PromocionesAdmin({ onScroll }: PageProps) {
                     <section aria-labelledby="promotions-heading" className="mt-6">
                         <h2 id="promotions-heading" className="sr-only">Promociones y Combos</h2>
                         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                            <MainForm
-                                actionType="post-promociones"
-                                dataForm={promocionesField()}
-                                message_button="Registrar"
-                            />
+                            {filteredData.length > 0
+                                ? filteredData.map((item: any) => (
+                                    item.nombreCombo
+                                        ? <ComboCard combo={item} key={item.id || item.nombreCombo} />
+                                        : <ProductoCard promocion={item} key={item.id || item.nombre} />
+                                ))
+                                : (
+                                    <div className="m-auto col-span-3 text-center py-8">
+                                        <p className="text-gray-500 italic">
+                                            Actualmente no hay promociones disponibles, vuelve en otro momento.
+                                        </p>
+                                    </div>
+                                )
+                            }
                         </ul>
                     </section>
+                    <ModalAddPromotion isOpen={isOpen} setIsOpen={setIsOpen} form={promocionesField()} />
                 </div>
             </main>
+
             <Footer />
         </IonContent>
     );
