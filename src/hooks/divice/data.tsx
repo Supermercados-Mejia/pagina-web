1100f5be5a2e20ac8fced833e780ef4b6f951587
Juan Jose Eusebio Rodriguez <juan.eusebio@devclimbers.com>
2025-06-01 19:58:18 -0700

    feat: exportar funciones de verificación de red y obtención de información del dispositivo

--- src/hooks/divice/data.tsx
+++ src/hooks/divice/data.tsx
@@ -1,37 +1,17 @@
-import { useEffect, useState } from 'react';
 import { Device } from '@capacitor/device';
 
-const DeviceInfoComponent = () => {
-    const [deviceInfo, setDeviceInfo] = useState<any>(null);
-
-    useEffect(() => {
-        const getDeviceInfo = async () => {
-            const info = await Device.getInfo();
-            const deviceId: any = await Device.getId();
-            setDeviceInfo({
-                ...info,
-                deviceId: deviceId.uuid, // UUID único del dispositivo
-            });
-        };
-
-        getDeviceInfo();
-    }, []);
-
-    return (
-        <div>
-            {deviceInfo ? (
-                <div>
-                    <p>Modelo: {deviceInfo.model}</p>
-                    <p>Sistema Operativo: {deviceInfo.operatingSystem}</p>
-                    <p>Versión del OS: {deviceInfo.osVersion}</p>
-                    <p>Fabricante: {deviceInfo.manufacturer}</p>
-                    <p>UUID: {deviceInfo.deviceId}</p>
-                </div>
-            ) : (
-                <p>Cargando información del dispositivo...</p>
-            )}
-        </div>
-    );
+export const getDeviceInfo = async () => {
+    const info = await Device.getInfo();
+    const deviceId: any = await Device.getId();
+    return ({
+        ...info,
+        deviceId: deviceId.uuid, // UUID único del dispositivo
+    });
 };
-
-export default DeviceInfoComponent;
+/* 
+    <p>Modelo: {deviceInfo.model}</p>
+    <p>Sistema Operativo: {deviceInfo.operatingSystem}</p>
+    <p>Versión del OS: {deviceInfo.osVersion}</p>
+    <p>Fabricante: {deviceInfo.manufacturer}</p>
+    <p>UUID: {deviceInfo.deviceId}</p>
+ */
