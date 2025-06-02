225cb60e91789157cff527f77ed124d02bdc7511
Juan Jose Eusebio Rodriguez <juan.eusebio@devclimbers.com>
2025-06-01 22:02:02 -0700

    feat: agregar ruta para promociones en el menú de navegación

--- src/pages/@promociones/[id]/layout.tsx
+++ src/pages/@promociones/[id]/layout.tsx
@@ -1,5 +1,4 @@
 import type React from "react";
-import { useState } from "react";
 import { IonPage } from "@ionic/react";
 import Header from "@/template/header";
 import AppMenu from "@/template/menu";
