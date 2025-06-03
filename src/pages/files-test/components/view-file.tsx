36ea2aed865d2129eaa650f52b83668dfe5354b8
Juan José Eusebio <juanerpgr@gmail.com>
2025-06-03 13:59:05 -0700

    feat: agregar lógica para seleccionar archivos en el componente CardFile y ajustar el diseño en ViewFile

--- src/pages/files-test/components/view-file.tsx
+++ src/pages/files-test/components/view-file.tsx
@@ -1,12 +1,18 @@
 import { IonList } from "@ionic/react";
 import { CardFile } from "./card-file";
+import { useState } from "react";
 
 export default function ViewFile() {
+    const [file, setFile] = useState()
     return (
-        <div className="view-file">
+        <section className="flex items-center justify-center w-full h-full p-4 gap-4">
             <IonList>
-                <CardFile />
+                <CardFile selectFile={(file: any) => setFile(file)} />
             </IonList>
-        </div>
+
+            <div className="text-center mt-4 border rounded-lg p-4 bg-gray-100">
+                <p className="text-gray-600">No hay más archivos para mostrar.</p>
+            </div>
+        </section>
     );
 } 
