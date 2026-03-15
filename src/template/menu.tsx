a8c530b88f53403388dea9dc6a8ac139c59ce60b
Aleida Rodriguez <aleida.rdriguez@gmail.com>
2026-03-15 09:05:14 -0700

    refactor: clean up unused code and improve component structure in page.tsx and menu.tsx

--- src/template/menu.tsx
+++ src/template/menu.tsx
@@ -135,16 +135,7 @@ const AppMenu = () => {
                             </p>
                         </div>
                     )}
-                    {!user && (
-                        <div className="p-4 text-sm text-gray-500">
-                            <p>
-                                Ingresa a nuestra pagina de compras en linea:
-                                <a href="https://dev.mercadosliz.com/productos" className="text-primary-600" target="_blank">
-                                    PickUp
-                                </a>
-                            </p>
-                        </div>
-                    )}
+                    
                 </IonContent>
                 <IonModal
                     isOpen={isOpen}
